38d457281aa6acc0698d6109455bb79182419551
ongshaoyu8825 <ongshaoyu8825@gmail.com>
2025-08-28 21:16:37 +0800

    set chatbot name as Ben add intro and outro messages

--- src/main/java/Ben.java
+++ src/main/java/Ben.java
@@ -1,7 +1,7 @@
-public class Bobby {
+public class Ben {
     public static void main(String[] args) {
         String intro = "____________________________________________________________\n" +
-                " Hello! I'm El\n" +
+                " Hello! I'm Ben\n" +
                 " What can I do for you?\n" +
                 "____________________________________________________________\n";
         String outro = " Bye. Hope to see you again soon!\n" +
