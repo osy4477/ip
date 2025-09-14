66dde291876437239705c04fc0c1ee3180da8d27
ongshaoyu8825 <129574664+osy4477@users.noreply.github.com>
2025-09-14 11:27:51 +0800

    Changed name to Ben and added greetings.

--- src/main/java/Ben.java
+++ src/main/java/Ben.java
@@ -4,9 +4,11 @@ public class Ben {
                 " Hello! I'm Ben\n" +
                 " What can I do for you?\n" +
                 "____________________________________________________________\n";
+        String content = "content\n";
         String outro = " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________";
         System.out.println(intro);
+        System.out.println(content);
         System.out.println(outro);
     }
 }
