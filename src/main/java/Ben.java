9963cf6c2988ced7809ac8d56138faccd6fdf5dd
ongshaoyu8825 <129574664+osy4477@users.noreply.github.com>
2025-09-17 21:02:02 +0800

    Add feature to mark tasks as done and unmark tasks as undone

--- src/main/java/Ben.java
+++ src/main/java/Ben.java
@@ -13,29 +13,39 @@ public class Ben {
 
         System.out.println(greeting);
         Scanner scanner = new Scanner(System.in);
-        String[] tasks = new String[100]; // fixed-size array
-        int taskCount = 0;                // number of tasks stored
+        Task[] tasks = new Task[100];
+        int taskCount = 0;
 
         while (true) {
             String input = scanner.nextLine();
+            String[] words = input.split(" ", 2);
 
             if (input.equals("bye")) {
                 System.out.println(byeMessage);
                 break;
             } else if (input.equals("list")) {
-                //display stored tasks
-                StringBuilder output = new StringBuilder();
+                StringBuilder output = new StringBuilder("Here are the tasks in your list:\n");
                 for (int i = 0; i < taskCount; i++) {
-                    output.append((i + 1) + ". " + tasks[i] + "\n");
+                    output.append((i + 1) + "." + tasks[i] + "\n");
                 }
-                //remove last newline if exists
-                if (output.length() > 0) {
-                    output.setLength(output.length() - 1);
+                if (output.charAt(output.length() - 1) == '\n') {
+                    output.setLength(output.length() - 1); // remove trailing newline
                 }
                 System.out.println(output.toString());
+
+            } else if (words[0].equals("mark")) {
+                int index = Integer.parseInt(words[1]) - 1;
+                tasks[index].markDone();
+                System.out.println("Nice! I've marked this task as done:\n  " + tasks[index]);
+
+            } else if (words[0].equals("unmark")) {
+                int index = Integer.parseInt(words[1]) - 1;
+                tasks[index].markNotDone();
+                System.out.println("OK, I've marked this task as not done yet:\n  " + tasks[index]);
+
             } else {
-                //add new task
-                tasks[taskCount] = input;
+                // Add task
+                tasks[taskCount] = new Task(input);
                 taskCount++;
                 System.out.println("added: " + input);
             }
