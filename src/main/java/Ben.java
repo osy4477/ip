08a9f31509ee8365f3fa3cd926cf7895b48b5627
ongshaoyu8825 <129574664+osy4477@users.noreply.github.com>
2025-09-17 20:38:10 +0800

    Add feature that echos user inputs Add bye message when user types the word bye

--- src/main/java/Ben.java
+++ src/main/java/Ben.java
@@ -1,14 +1,27 @@
+import java.util.Scanner;
+
 public class Ben {
     public static void main(String[] args) {
-        String intro = "____________________________________________________________\n" +
+
+        String greeting = "____________________________________________________________\n" +
                 " Hello! I'm Ben\n" +
                 " What can I do for you?\n" +
                 "____________________________________________________________\n";
-        String content = "content\n";
-        String outro = " Bye. Hope to see you again soon!\n" +
+        String byeMessage = " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________";
-        System.out.println(intro);
-        System.out.println(content);
-        System.out.println(outro);
+
+        System.out.println(greeting);
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println(byeMessage);
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
     }
 }
