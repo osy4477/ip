6294bc471e18622a3ef35484e28be37f714acbe3
ongshaoyu8825 <129574664+osy4477@users.noreply.github.com>
2025-09-17 21:03:59 +0800

    Add Task class to represent tasks added by user

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
-
 public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false; // default not done
+    }
+
+    public void markDone() {
+        isDone = true;
+    }
+
+    public void markNotDone() {
+        isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
 }
